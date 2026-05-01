879f854bfe7266ae2f346cfd280077518b8adae5
ROMERUU-dev <juvenal.romero@uabc.edu.mx>
2026-04-30 17:43:54 -0700

    Align Magic top cell with tapeout module

--- mag/magic_init_project.tcl
+++ mag/magic_init_project.tcl
@@ -8,7 +8,7 @@
 
 # Change the settings below to match your design:
 # ------------------------------------------------
-set TOP_LEVEL_CELL     tt_um_Rog-helio
+set TOP_LEVEL_CELL     tt_um_rog_helio_
 set TEMPLATE_FILE      tt_analog_1x2.def
 set POWER_STRIPE_WIDTH 2um                 ;# The minimum width is 1.2um
 
